6938c16cc173b42b7cf913cf64155adb6c316cb8
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-01-05 11:49:56 +0100

    editing home page

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { planes } from './Planes'
 
 function Home() {
     return (
-        <div>
-
+        <div className='bg-white w-[75%] flex justify-center items-center absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2 rounded-lg'>
+            <div className='flex justify-center items-center p-5 w-full h-full'>
+                <div className='grid grid-cols-4 gap-5'>
+                    {planes.map((plane) => {
+                        return (
+                            <div
+                                key={plane.id}
+                                className='pb-2 w-60 border-solid border-[1px] border-black flex justify-center items-center flex-col gap-2 rounded-lg h-[200px]'
+                            >
+                                <img
+                                    className='w-full h-full rounded-b-none rounded-t-lg'
+                                    src={plane.img}
+                                    alt={plane.name} />
+                                <h1>{plane.name}</h1>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
