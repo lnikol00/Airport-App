0dc2c09bb1774c07043622655dbd9ae4bdac117d
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-01-10 23:00:04 +0100

    fixing repsosivne design

--- client/src/pages/create/Create.tsx
+++ client/src/pages/create/Create.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Create() {
     return (
-        <div className='flex flex-col justify-center items-center pt-5'>
-            <h2 className='text-3xl pb-5'>Add new plane</h2>
-            <form className='flex flex-col justify-center items-center gap-[20px]'>
+        <div className='flex flex-col justify-center items-center md:pt-5 pt-3'>
+            <h2 className='md:text-3xl text-xl md:pb-5 pb-2'>Add new plane</h2>
+            <form className='flex flex-col justify-center items-center md:gap-[20px] gap-[15px]'>
                 <label className='relative' id='model'>
                     <input
                         type='text'
@@ -12,6 +12,14 @@ function Create() {
                         className='h-[45px] md:w-[450px] w-80 px-4 text-md bg-inherit border-2 border-opacity-50 border-black rounded-lg outline-none focus:border-white focus:text-white transition duration-200'
                     />
                     <span className='text-md text-black absolute left-0 top-[10px] mx-6 px-2 transition duration-200 input-text'>Model</span>
+                </label>
+                <label className='relative' id='image'>
+                    <input
+                        type='text'
+                        required
+                        className='h-[45px] md:w-[450px] w-80 px-4 text-md bg-inherit border-2 border-opacity-50 border-black rounded-lg outline-none focus:border-white focus:text-white transition duration-200'
+                    />
+                    <span className='text-md text-black absolute left-0 top-[10px] mx-6 px-2 transition duration-200 input-text'>Image</span>
                 </label>
                 <label className='relative' id='year'>
                     <input
