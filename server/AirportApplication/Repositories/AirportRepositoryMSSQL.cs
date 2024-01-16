c4471b0871991859f89c507b20da6ee39f4b9bef
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-01-16 21:33:02 +0100

    Adding new repository for MSSQL database

---
+++ server/AirportApplication/Repositories/AirportRepositoryMSSQL.cs
@@ -0,0 +1,32 @@
+﻿using AirportApplication.Models;
+
+namespace AirportApplication.Repositories
+{
+    public class AirportRepositoryMSSQL : IAirportRepository
+    {
+        public void CreateNewPlane(Airport plane)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePlane(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Airport> GetAllPlanes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Airport? GetSinglePlane(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePlane(int id, Airport updatedPlane)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
