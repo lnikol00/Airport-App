f98afd15a61dca656c5c687b0a643e7adbe8103b
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-01-17 18:00:04 +0100

    Adding messages

---
+++ client/src/utils/messages/Toast.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { ToastContainer } from 'react-toastify';
+// import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Toast = () => {
+    return (
+        <div>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+        </div>
+    )
+}
+
+export default Toast
