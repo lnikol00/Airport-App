4282af1a8a04b6365f61df0b4c796a7f699980d7
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2024-01-17 18:00:20 +0100

    Update interface

--- client/src/interfaces/types.ts
+++ client/src/interfaces/types.ts
@@ -1,6 +1,7 @@
 export type Plane = {
     id: number
     model: string
+    name: string
     year: number
     country: string
     capacity: number
@@ -9,11 +10,7 @@ export type Plane = {
     captain: string
 }
 
-export type PlaneListState = {
-    planes: Plane[]
-    loading?: boolean
-    error?: string
-}
+export type PlaneList = Array<Plane>
 
 export type PaginationProps = {
     totalPlanes: number
